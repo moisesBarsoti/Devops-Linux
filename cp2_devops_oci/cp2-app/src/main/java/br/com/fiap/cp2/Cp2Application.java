871dac6c6090c6af2bf9ee8d61d167c20d1cbe02
moisesBarsoti <moises.barsot2@gmail.com>
2026-04-26 18:42:08 -0300

    feat(Cp2Application): feito endpoint healthCheck

--- cp2_devops_oci/cp2-app/src/main/java/br/com/fiap/cp2/Cp2Application.java
+++ cp2_devops_oci/cp2-app/src/main/java/br/com/fiap/cp2/Cp2Application.java
@@ -2,12 +2,21 @@ package br.com.fiap.cp2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Cp2Application {
+
+    record HealthStatus(String status, String message){}
 
     public static void main(String[] args) {
         SpringApplication.run(Cp2Application.class, args);
     }
 
+    @GetMapping("/")
+    public HealthStatus healthCheack() {
+        return new HealthStatus("Ok", "Api is run");
+    }
 }
